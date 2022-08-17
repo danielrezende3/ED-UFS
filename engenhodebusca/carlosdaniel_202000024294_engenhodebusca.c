ad336062549017c41dac70ff36757a6bb3ccb187
Daniel Rezende <danielrezende2@academico.ufs.br>
2022-08-17 18:07:36 -0300

    commit

--- engenhodebusca/carlosdaniel_202000024294_engenhodebusca.c
+++ engenhodebusca/carlosdaniel_202000024294_engenhodebusca.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     // Declaracao de variaveis
     FILE *pFile_input;
     FILE *pFile_output;
-    int n;
+    int total;
     int valor;
     int servidores;
     int capacidade_maxima;
@@ -36,25 +36,9 @@ int main(int argc, char *argv[])
     }
 
     fscanf(pFile_input, "%i %i", &servidores, &capacidade_maxima);
-    fprintf(pFile_output, "%i %i\n", servidores, capacidade_maxima);
     fgetc(pFile_input);
-    fscanf(pFile_input, "%i", &n);
+    fscanf(pFile_input, "%i", &total);
     fgetc(pFile_input);
-    for (int i = 0; i < n; i++)
-    {
-        fscanf(pFile_input, "%i", &n);
-        fprintf(pFile_output, "%i\n", n);
-
-        for (int j = 0; j < n; j++)
-        {
-            fscanf(pFile_input, "%i", &valor);
-            fprintf(pFile_output, "%i ", valor);
-            fgetc(pFile_input);
-        }
-        fprintf(pFile_output, "\n");
-        fgetc(pFile_input);
-    }
-
     // Fechando arquivos
     fclose(pFile_input);
     fclose(pFile_output);
