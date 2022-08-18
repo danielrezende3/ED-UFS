8de5657f6e111cf919b634a303711a756406d91b
Daniel Rezende <danielrezende2@academico.ufs.br>
2022-08-18 14:58:11 -0300

    Testing Changes to be committed: 	modified:   redesocial/redesocial.output 	modified:   sistemadeimpressao/carlosdaniel_202000024294_sistemadeimpressao.c

--- sistemadeimpressao/carlosdaniel_202000024294_sistemadeimpressao.c
+++ sistemadeimpressao/carlosdaniel_202000024294_sistemadeimpressao.c
@@ -104,12 +104,12 @@ int main(int argc, char *argv[])
     int total_impressoras;
     int total_paginas = 0;
     // Abertura de arquivos
-    pFile_input = fopen("sistemadeimpressao.input", "r");
+    pFile_input = fopen(argv[1], "r");
     if (pFile_input == NULL)
     {
         return 1;
     }
-    pFile_output = fopen("sistemadeimpressao.output", "w");
+    pFile_output = fopen(argv[2], "w");
     if (pFile_output == NULL)
     {
         return 1;
