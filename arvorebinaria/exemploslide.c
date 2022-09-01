d08c441c755497e916d780a010f8ccce1ee04d37
Daniel Rezende <danielrezende2@academico.ufs.br>
2022-09-01 18:34:02 -0300

    Feito o exemplo de exclusão de árvore binária

---
+++ arvorebinaria/exemploslide.c
@@ -0,0 +1,138 @@
+// Comecei a estudar 0514
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef struct BstNode
+{
+    int data;
+    struct BstNode *left;
+    struct BstNode *right;
+} BstNode;
+
+BstNode *GetNewNode(int data)
+{
+    BstNode *newNode = (BstNode *)malloc(sizeof(BstNode));
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+BstNode *Insert(BstNode *root, int data)
+{
+    if (root == NULL)
+    {
+        root = GetNewNode(data);
+        return root;
+    }
+    else if (data <= root->data)
+    {
+        root->left = Insert(root->left, data);
+    }
+    else
+    {
+        root->right = Insert(root->right, data);
+    }
+
+    return root;
+}
+
+BstNode *FindMin(BstNode *root)
+{
+    while (root->left != NULL)
+        root = root->left;
+    return root;
+}
+
+int Search(BstNode *root, int data)
+{
+    if (root == NULL)
+    {
+        return FALSE;
+    }
+    else if (root->data == data)
+    {
+        return TRUE;
+    }
+    else if (data <= root->data)
+    {
+        return Search(root->left, data);
+    }
+    else
+    {
+        return Search(root->right, data);
+    }
+}
+
+struct BstNode *Delete(struct BstNode *root, int data)
+{
+    if (root == NULL)
+        return root;
+    else if (data < root->data)
+        root->left = Delete(root->left, data);
+    else if (data > root->data)
+        root->right = Delete(root->right, data);
+    // Wohoo... I found you, Get ready to be deleted
+    else
+    {
+        // Case 1:  No child
+        if (root->left == NULL && root->right == NULL)
+        {
+            free(root);
+            root = NULL;
+        }
+        // Case 2: One child
+        else if (root->left == NULL)
+        {
+            struct BstNode *temp = root;
+            root = root->right;
+            free(temp);
+        }
+        else if (root->right == NULL)
+        {
+            struct BstNode *temp = root;
+            root = root->left;
+            free(temp);
+        }
+        // case 3: 2 children
+        else
+        {
+            struct BstNode *temp = FindMin(root->right);
+            root->data = temp->data;
+            root->right = Delete(root->right, temp->data);
+        }
+    }
+    return root;
+}
+
+int main(int argc, char *argv[])
+{
+    int total_numbers, value_of_node, total_deletes;
+    BstNode *root = NULL;
+    FILE *pInput;
+    pInput = fopen("professor.input", "r");
+    if (pInput == NULL)
+    {
+        return 1;
+    }
+    fscanf(pInput, "%i", &total_numbers);
+    fgetc(pInput);
+    for (int i = 0; i < total_numbers; i++)
+    {
+        fscanf(pInput, "%i", &value_of_node);
+        root = Insert(root, value_of_node);
+    }
+    fscanf(pInput, "%i", &total_deletes);
+    fgetc(pInput);
+    for (int i = 0; i < total_deletes; i++)
+    {
+        fscanf(pInput, "%i", &value_of_node);
+        root = Delete(root, value_of_node);
+    }
+
+    return 0;
+e}
