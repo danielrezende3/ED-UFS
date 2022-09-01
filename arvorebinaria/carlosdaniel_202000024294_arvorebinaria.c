bc978cca4162c9b41d3f668ed14a0a5585294906
Daniel Rezende <danielrezende2@academico.ufs.br>
2022-09-01 18:34:04 -0300

    Feito processo de deletar node na árvore binária

--- arvorebinaria/carlosdaniel_202000024294_arvorebinaria.c
+++ arvorebinaria/carlosdaniel_202000024294_arvorebinaria.c
@@ -2,7 +2,31 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    // Declaração de variáveis
+    int total;
+    // Abertura de arquivos
+    FILE *pFile_input;
+    FILE *pFile_output;
+    // TODO: Trocar "teste.input" por "argv[1]"
+    pFile_input = fopen("teste.input", "r");
+    if (pFile_input == NULL)
+    {
+        return 1;
+    }
+    // TODO: Trocar "meu.output" por "argv[2]"
+    pFile_output = fopen("meu.output", "w");
+    if (pFile_output == NULL)
+    {
+        return 1;
+    }
+    fscanf(pFile_input, "%i", &total);
+    fgetc(pFile_input);
+    for (int i = 0; i < total; i++)
+    {
+        
+    }
     
     return 0;
 }
