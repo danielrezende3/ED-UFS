d499880f5265cb9c25e65b7c4d7b77519cc5f999
Daniel Rezende <calor.danileu@gmail.com>
2022-09-20 18:42:23 -0300

    csontrução dicionario

--- dicionario/carlosdaniel_202000024294_dicionario.c
+++ dicionario/carlosdaniel_202000024294_dicionario.c
@@ -4,16 +4,18 @@
 
 #define WORD_LENGTH 31 // palavra(30) + 1\0
 #define TOTAL_SINONIMOS 10
-#define SINONIMO_LENGTH (WORD_LENGTH*TOTAL_SINONIMOS) // ((palavra) 30 + 1 (espaco)) x 10
-#define TOTAL_LENGTH (WORD_LENGTH+SINONIMO_LENGTH) // word_length + sinonimo_length
+#define SINONIMO_LENGTH (WORD_LENGTH * TOTAL_SINONIMOS) // ((palavra) 30 + 1 (espaco)) x 10
+#define TOTAL_LENGTH (WORD_LENGTH + SINONIMO_LENGTH)    // word_length + sinonimo_length
 
+FILE *pOutput;
 char linha[TOTAL_LENGTH];
+char palavra2[SINONIMO_LENGTH];
 
 struct Node
 {
     int height;
     char palavra[WORD_LENGTH];
-    char sinonimos[TOTAL_SINONIMOS][WORD_LENGTH];
+    char sinonimos[SINONIMO_LENGTH];
     struct Node *left;
     struct Node *right;
 };
@@ -79,6 +81,7 @@ struct Node *newNode(char data[])
 {
     struct Node *node = (struct Node *)malloc(sizeof(struct Node));
     memcpy(node->palavra, data, WORD_LENGTH);
+    memcpy(node->sinonimos, palavra2, SINONIMO_LENGTH);
     node->left = NULL;
     node->right = NULL;
     node->height = 1;
@@ -93,11 +96,11 @@ struct Node *insert(struct Node *node, char data[])
     {
         return (newNode(data));
     }
-    if (memcmp(node->palavra, data, strlen(data)) < 0)
+    if (memcmp(node->palavra, data, WORD_LENGTH) < 0)
     {
         node->left = insert(node->left, data);
     }
-    else if (memcmp(node->palavra, data, strlen(data)) > 0)
+    else if (memcmp(node->palavra, data, WORD_LENGTH) > 0)
     {
         node->right = insert(node->right, data);
     }
@@ -114,22 +117,23 @@ struct Node *insert(struct Node *node, char data[])
     // there are 4 cases
 
     // Left Left Case
-    if (balance > 1 && data < node->left->palavra)
+
+    if (balance > 1 && memcmp(node->left->palavra, data, WORD_LENGTH) < 0)
         return rightRotate(node);
 
     // Right Right Case
-    if (balance < -1 && data > node->right->palavra)
+    if (balance < -1 && memcmp(node->right->palavra, data, WORD_LENGTH) > 0)
         return leftRotate(node);
 
     // Left Right Case
-    if (balance > 1 && data > node->left->palavra)
+    if (balance > 1 && memcmp(node->left->palavra, data, WORD_LENGTH) > 0)
     {
         node->left = leftRotate(node->left);
         return rightRotate(node);
     }
 
     // Right Left Case
-    if (balance < -1 && data < node->right->palavra)
+    if (balance < -1 && memcmp(node->left->palavra, data, WORD_LENGTH) < 0)
     {
         node->right = rightRotate(node->right);
         return leftRotate(node);
@@ -137,6 +141,27 @@ struct Node *insert(struct Node *node, char data[])
 
     /* return the (unchanged) node pointer */
     return node;
+}
+struct Node *search(struct Node *node, char data[])
+{
+    if (node == NULL)
+    {
+        // fprintf(pOutput, "-\n");
+        printf("-\n");
+        return NULL;
+    }
+    if (strcmp(node->palavra, data) == 0)
+    {
+        printf("rodei!");
+    }
+    if (strcmp(node->palavra, data) < 0)
+    {
+        node->left = search(node->left, data);
+    }
+    else if (strcmp(node->palavra, data) > 0)
+    {
+        node->right = search(node->right, data);
+    }
 }
 
 int main(int argc, char const *argv[])
@@ -147,10 +172,11 @@ int main(int argc, char const *argv[])
     char palavra1[WORD_LENGTH];
     char *token = NULL;
 
+    total_words = 0;
+    total_searchs = 0;
     struct Node *root = NULL;
 
     FILE *pInput;
-    FILE *pOutput;
     // TODO: Trocar "teste.input" por argv[1]
     pInput = fopen("slide.input", "r");
     if (pInput == NULL)
@@ -164,27 +190,31 @@ int main(int argc, char const *argv[])
         return 1;
     }
 
-    fscanf(pInput, "%i", &total_words);
+    if (fscanf(pInput, "%i", &total_words))
+        ;
     fgetc(pInput);
     // pega o total de palavras a serem adicionadas, faz um loop com a linha inteira,
-    // Separa a primeira palavra, retira o número, e coloca em "sinonimo[]" 
+    // Separa a primeira palavra, retira o número, e coloca em "sinonimo[]"
     for (int i = 0; i < total_words; i++)
     {
-        fscanf(pInput, "%[^\n]", linha);
+        if (fscanf(pInput, "%[^\n]", linha))
+            ;
         memcpy(palavra1, linha, WORD_LENGTH);
         strtok_r(palavra1, espaco, &token);
+        memcpy(palavra2, linha + strlen(palavra1) + 3, SINONIMO_LENGTH);
         root = insert(root, palavra1);
         // sempre fica por último
         fgetc(pInput);
     }
-    fscanf(pInput, "%i", &total_searchs);
+    if (fscanf(pInput, "%i", &total_searchs))
+        ;
     fgetc(pInput);
     for (int i = 0; i < total_searchs; i++)
     {
-        // TODO
+        if (fscanf(pInput, "%[^\n]", linha))
+            ;
+        search(root, linha);
     }
-    
-    
+
     return 0;
 }
-
